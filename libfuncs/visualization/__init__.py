93eb63ea5b0c4aa59de214b67d925961b809d43c
Natural <76610619+itsmrnatural@users.noreply.github.com>
2025-03-08 12:20:56 +0000

    feat: add visualization module with plotting functions to libfuncs library

---
+++ libfuncs/visualization/__init__.py
@@ -0,0 +1,23 @@
+"""Visualization functionality for the libfuncs library."""
+
+from .plotting import (
+    plot_function,
+    plot_derivative,
+    plot_with_extrema,
+    plot_integral,
+    plot_multiple_functions,
+    plot_3d_function,
+    plot_contour,
+    plot_vector_field,
+)
+
+__all__ = [
+    'plot_function',
+    'plot_derivative',
+    'plot_with_extrema',
+    'plot_integral',
+    'plot_multiple_functions',
+    'plot_3d_function',
+    'plot_contour',
+    'plot_vector_field',
+]
